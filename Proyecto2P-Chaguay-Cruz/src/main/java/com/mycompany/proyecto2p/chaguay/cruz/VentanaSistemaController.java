fb9c6f600e3dddaeb79e6cfd7398abbb162ecc47
HP <jchaguay@espol.edu.ec>
2023-01-14 22:05:22 -0500

    Ingreso de usuarios

--- Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaSistemaController.java
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaSistemaController.java
@@ -77,30 +77,25 @@ public class VentanaSistemaController implements Initializable {
     
     @FXML
     void IngresarOptions(ActionEvent event) {
-        
-        Object evt =event.getSource();
-        if(evt.equals(btnIngresar)){
-            if(!txtUsuario.getText().isEmpty() && !txtContraseña.getText().isEmpty()){
-                String user= txtUsuario.getText();
-                String passwrd=txtContraseña.getText();
-                
-            }
-        }
-
-        
+                       
     }
-    static ArrayList<Usuario> listUsuario=new ArrayList<>();
-    
-    public void validarUsuario(){
-        try (BufferedReader bfr = new BufferedReader(new FileReader(Principal.pathFiles+"Usuarios.txt"))) {
-                String linea;
-                while((linea=bfr.readLine())!=null){
-                    
-                }
-
-            } catch (IOException e) {
-            }
-    }
+//    static ArrayList<Usuario> listUsuario=new ArrayList<>();
+//    
+//    public void validarUsuario(){
+//        try (BufferedReader bfr = new BufferedReader(new FileReader(Principal.pathFiles+"Usuarios.txt"))) {
+//                String linea;
+//                while((linea=bfr.readLine())!=null){
+//                    String[] lineas=linea.strip().split(";");
+//                            String user=lineas[0];
+//                            String psswrd=lineas[1];
+//                            listUsuario=new ArrayList<>();
+//                            Usuario n=new Usuario(user,psswrd);
+//                            listUsuario.add(n);
+//                }
+//
+//            } catch (IOException e) {
+//            }
+//    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
