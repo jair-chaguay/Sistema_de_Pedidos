a867cf20d2db1c4bc64cbaad887fe496329b0c95
angmcruz <angmcruz@espol.edu.ec>
2023-01-19 23:00:39 -0500

    creando ventana

--- Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaPedidoController.java
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaPedidoController.java
@@ -4,9 +4,15 @@
  */
 package com.mycompany.proyecto2p.chaguay.cruz;
 
+import com.mycompany.proyecto2p.chaguay.cruz.modelo.Menu;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  *
@@ -17,5 +23,52 @@ public class VentanaPedidoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }  
+    
+    @FXML
+    private VBox root;
+    
+    @FXML
+    private HBox seccionDatos;
+    
+    @FXML
+    private Label lbldatos;
+    
+    @FXML
+    private Label lblDescripcion;
+    
+    @FXML
+    private HBox ContButton;
+    
+    @FXML
+    private Label lblPrecio;
+    
+    @FXML
+    private Label lblCantidad;
+    
+    @FXML
+    private ComboBox<Menu> cbxmenu;
+    
+    
+    @FXML
+    private ComboBox<Menu> cbxordenar;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
