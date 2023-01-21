29782196c42a334b9a6286f80d9e0af7e237bb16
HP <jchaguay@espol.edu.ec>
2023-01-20 19:18:46 -0500

    Metodo para leer el archivo Locales.txt

--- Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaMapaController.java
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaMapaController.java
@@ -4,7 +4,12 @@
  */
 package com.mycompany.proyecto2p.chaguay.cruz;
 
+import com.mycompany.proyecto2p.chaguay.cruz.modelo.Locales;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
@@ -18,9 +23,33 @@ public class VentanaMapaController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    static ArrayList<Locales> local;
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+    
+    public static ArrayList<Locales>leerLocales(){
+        local=new ArrayList<>();
+        try(BufferedReader bfr= new BufferedReader(new FileReader(Principal.pathFiles+"Locales.txt"))){
+            String linea;
+            while((linea=bfr.readLine())!=null){
+                String[] lineas=linea.trim().strip().split(",");
+                String nombre=lineas[0];
+                String direccion=lineas[1];
+                String horario=lineas[2];
+                int coordX=Integer.parseInt(lineas[3]);
+                int coordY=Integer.parseInt(lineas[4]);
+                Locales lcal=new Locales(nombre,direccion,horario,coordX,coordY);
+                local.add(lcal);
+                
+            }
+            
+        }catch(IOException ex){
+            
+        }
+        return null;
+    }
+   
     
 }
