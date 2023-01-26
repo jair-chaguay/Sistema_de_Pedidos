987e8c42150e0d45be31e3ef99b23e942223b62c
angmcruz <angmcruz@espol.edu.ec>
2023-01-25 19:50:44 -0500

    metodo para leer txt

--- Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/modelo/Menu.java
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/modelo/Menu.java
@@ -1,12 +1,17 @@
-
 package com.mycompany.proyecto2p.chaguay.cruz.modelo;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Menu {
+
     String descripcion;
     double precio;
     String tipo;
-    
+
     public Menu(String descripcion, double precio, String tipo) {
         this.descripcion = descripcion;
         this.precio = precio;
@@ -41,17 +46,26 @@ public class Menu {
     public String toString() {
         return "Menu{" + "descripcion=" + descripcion + ", precio=" + precio + ", tipo=" + tipo + '}';
     }
-    
-    
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    public static ArrayList<Menu> leerArchivo() {
+        try ( BufferedReader bfr = new BufferedReader(new FileReader("Menu.txt"))) {
+            ArrayList<Menu> menu = new ArrayList<>();
+            String linea;
+            Menu m;
+            while((linea=bfr.readLine())!=null){
+                String[] lineas=linea.trim().strip().split(",");
+                Menu mn= new Menu(lineas[0],Double.parseDouble(lineas[1]),lineas[2]);
+                menu.add(mn);
+            
+            
+            }
+            return menu;
+
+        } catch (IOException ex) {
+            ex.getMessage();
+            return null;
+        }
+
+    }
+
 }
