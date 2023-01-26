1a4983844753abbcfef728553ff829918a99e970
angmcruz <angmcruz@espol.edu.ec>
2023-01-25 20:14:12 -0500

    creando ventana  haz pedido

---
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/VentanaPagoController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto2p.chaguay.cruz;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+
+/**
+ *
+ * @author mcruz
+ */
+public class VentanaPagoController implements Initializable {
+
+   
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+        // TODO
+    }
+}
