16afbc297b6108d26c2c1db3f4bcd98b140be0ba
angmcruz <angmcruz@espol.edu.ec>
2023-01-25 22:32:59 -0500

    CREACION CLASES PAGO Y PEDIDO

--- Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/modelo/Pedido.java
+++ Proyecto2P-Chaguay-Cruz/src/main/java/com/mycompany/proyecto2p/chaguay/cruz/modelo/Pedido.java
@@ -13,6 +13,48 @@ public class Pedido {
     private int cantidad; //cantidad de comida escogida
     private String nombreCliente;
     private double valor;
+
+    public Pedido(int idPedido, int cantidad, String nombreCliente, double valor) {
+        this.idPedido = idPedido;
+        this.cantidad = cantidad;
+        this.nombreCliente = nombreCliente;
+        this.valor = valor;
+    }
+
+    public int getIdPedido() {
+        return idPedido;
+    }
+
+    public void setIdPedido(int idPedido) {
+        this.idPedido = idPedido;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+    
+    
+    
     
   
     
